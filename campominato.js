9efead7392e2423ac56d884eb895a8663e0fb735
AndreaChiari <chiariandrea94@gmail.com>
2022-12-19 14:26:11 +0100

    update-problem-includes

--- campominato.js
+++ campominato.js
@@ -15,25 +15,41 @@ const col = 10
 const totalcells = (rows * col)+1;
 let score = 0
 
+// creo una function che mi permettta di generare numeri casuali UNICI
+
+let blacklist = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+const getRandomNumbers = (min = 1, max = 16, blacklist) =>{
+    let randomicNumbers = Math.floor(Math.random() * (max + 1 - min)) + min;
+    while (blacklist.includes(randomicNumbers)){;
+    console.log(randomicNumbers);
+   return randomicNumbers;
+   }
+ }
+
+ for(i=1; i <= 16; i++ ){
+
+     getRandomNumbers()
+ }
 
 // aggiungo un event listener per generare le caselle al click
 button.addEventListener('click', function(){
- 
-    for(i=1; i < totalcells; i++ ){
-        let cell = document.createElement('div');
-        cell.innerHTML = i
-        grid.appendChild(cell);
-        cell.classList.add('grid-cell');
+   
+   for(i=1; i < totalcells; i++ ){
+       let cell = document.createElement('div');
+       cell.innerHTML = i
+       grid.appendChild(cell);
+       cell.classList.add('grid-cell');
 
 // aggiungo un altro event listener per cambiare il colore delle caselle
-        cell.addEventListener('click',function(){
-            cell.classList.add('click-azure')
-            score += 1
-            cell.classList.add('click-stop')
-            console.log(cell.innerText,score)
-        },{once : true})
-        }
-    },{once : true})
+       cell.addEventListener('click',function(){
+           cell.classList.add('click-azure')
+           score += 1
+           cell.classList.add('click-stop')
+           console.log(cell.innerText,score)
+       },{once : true})
+       }
+   },{once : true})
+  
 
 
 
